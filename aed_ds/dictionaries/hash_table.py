8cca2fa13a809c2098f87ef1d48722699cfdc3df
dfrvieira <dfrvieira@users.noreply.github.com>
2020-06-03 21:40:50 +0100

    Update hash_table.py

--- aed_ds/dictionaries/hash_table.py
+++ aed_ds/dictionaries/hash_table.py
@@ -1,25 +1,113 @@
-from .tad_dictionary import Dictionary
 from ..exceptions import NoSuchElementException, DuplicatedKeyException
 from ..lists.singly_linked_list import SinglyLinkedList
+from .item import Item 
+
+import ctypes
 
 class HashTable(Dictionary):
+    
     def __init__(self, size=101):
-        pass
+        self.array_size = size
+        self.num_elements = 0
+        self.table = (self.array_size * ctypes.py_object)() # Array of pointers
 
-    def size(self): pass
+        # Create an empty list for each table position
+        for i in range(self.array_size):
+            self.table[i] = SinglyLinkedList()
 
-    def is_full(self): pass
+    def size(self):
+        return self.num_elements
 
-    def get(self, k): pass
+    def is_full(self):
+        return self.num_elements == self.array_size
 
-    def insert(self, k, v): pass
+    def get(self, k):
+        if not self.has_key(k):
+            raise NoSuchElementException()
+        else:
+            idx = self.table[self.hashfunction(k)]
+            for _ in range(self.size()):
+                node = idx.iterator().next()
+                if node.get_element().get_key() == k:
+                    return node.get_element().get_value()
 
-    def update(self, k, v): pass
+    def insert(self, k, v):
+        # Check if it has key
+        if self.has_key(k):
+            raise DuplicatedKeyException()
 
-    def remove(self, k): pass
+        ## Insert new item
+        # Calculate the table index
+        idx = self.hash_function(k) # O(1)
+        # Create a new Item
+        item = Item(k, v)
+        # Insert the item in the colision list
+        self.table[idx].insert_last(item)
+        # Update the number of elements
+        self.num_elements += 1
 
-    def keys(self): pass
+    def update(self, k, v):
+        if not self.has_key(k):
+            raise NoSuchElementException()
+        
+        else:
+            idx = self.table[self.hashfunction(k)]
+            for _ in range(self.size()):
+                node = idx.iterator().next()
+                if node.get_element().get_key == k:
+                    node.get_element().set_value(v)
 
-    def values(self): pass
+    def remove(self, k):
+        if not self.has_key(k):
+            raise NoSuchElementException()
+        
+        idx = self.hash_function(k)
+        list_colision = self.table[idx]
+        iterator = list_colision.iterator()
+        
+        while iterator.has_next():
+            current_item = iterator.next()
+            
+            if current_item.get_key() == k: 
+                position = list_colision.find(current_item)
+                list_colision.remove(position)
+                self.num_elements -= 1  # Update number of elements
+                
 
-    def items(self): pass
+        
+        
+        
+
+    def keys(self):
+        result = SinglyLinkedList()
+        for index in self.table:
+            while index.iterator().has_next():
+                result.insert_last(index.iterator().next().get_key())
+        return result
+    
+    def values(self):
+        result = SinglyLinkedList()
+        for index in self.table:
+            while index.iterator().has_next():
+                result.insert_last(index.iterator().next().get_value())
+        return result
+    
+    def items(self):
+        result = SinglyLinkedList()
+        for index in self.table:
+            while index.iterator().has_next():
+                result.insert_last(index.iterator().next())
+        return result
+
+    def hash_function(self, k):
+        return sum([ord(c) for c in k]) % self.array_size
+
+    def has_key(self, k):
+        idx = self.hash_function(k) # O(1)    
+        colision_list = self.table[idx]
+        it = colision_list.iterator()
+        while it.has_next():
+            current_item = it.next()
+            if current_item.get_key() == k:
+                return True
+        return False
